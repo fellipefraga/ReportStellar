42e687c4e6ff04919c5c7a240ef34a1017536d64
fellipefraga <fellipeedu@gmail.com>
2026-01-02 17:13:28 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { LandingPage } from './components/LandingPage';
+
+export default function App() {
+  return (
+    <div className="size-full">
+      <LandingPage />
+    </div>
+  );
+}
